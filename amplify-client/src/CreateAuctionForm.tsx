f643e6d39aae3b63a3194344e7e43b19f27127ac
Larrygf02 <raulgf_02@hotmail.com>
2020-08-01 11:38:41 -0500

    add mutation appsync in form

--- amplify-client/src/CreateAuctionForm.tsx
+++ amplify-client/src/CreateAuctionForm.tsx
@@ -2,7 +2,10 @@ import * as React from 'react'
 import { Formik } from 'formik'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-
+import { Mutation } from 'react-apollo';
+import { createAuction } from './graphql/mutations';
+import gql from 'graphql-tag'
+import { CreateAuctionMutation, CreateAuctionMutationVariables } from './API';
 interface FormValues {
   name: string,
   price: number
@@ -10,39 +13,44 @@ interface FormValues {
 
 export const CreateAuctionForm = () => {
     return (
-        <Formik<FormValues>
-            initialValues={{
-                name: "",
-                price:0
-            }} 
-            onSubmit={({name, price}) => {
-                // call mutation
-                console.log({name,price});
-            }}
-        >
-            {({values, handleChange, handleSubmit}) => (
-                <form onSubmit={handleSubmit}>
-                      <TextField
-                            name="name"
-                            label="Name"
-                            value={values.name}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <br></br>
-                        <TextField
-                            name="price"
-                            label="Price"
-                            value={values.price}
-                            onChange={handleChange}
-                            margin="normal"
-                        />
-                        <br/>
-                        <Button type="submit" variant="contained">
-                            Submit
-                        </Button>
-                </form>
+        <Mutation<CreateAuctionMutation, CreateAuctionMutationVariables> 
+                mutation={gql(createAuction)}>
+            {(createAuction) => (
+                <Formik<FormValues>
+                    initialValues={{
+                        name: "",
+                        price:0
+                    }} 
+                    onSubmit={({name, price}) => {
+                        // call mutation
+                        console.log({name,price});
+                    }}
+                >
+                    {({values, handleChange, handleSubmit}) => (
+                        <form onSubmit={handleSubmit}>
+                            <TextField
+                                    name="name"
+                                    label="Name"
+                                    value={values.name}
+                                    onChange={handleChange}
+                                    margin="normal"
+                                />
+                                <br></br>
+                                <TextField
+                                    name="price"
+                                    label="Price"
+                                    value={values.price}
+                                    onChange={handleChange}
+                                    margin="normal"
+                                />
+                                <br/>
+                                <Button type="submit" variant="contained">
+                                    Submit
+                                </Button>
+                        </form>
+                    )}
+                </Formik>
             )}
-        </Formik>
+        </Mutation>
     )
 }
