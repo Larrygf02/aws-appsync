9266b5209b27418c152b9e1bdd4e152703604b19
Larrygf02 <raulgf_02@hotmail.com>
2020-08-02 23:56:10 -0500

    add cards in auction

--- amplify-client/src/Auction.tsx
+++ amplify-client/src/Auction.tsx
@@ -3,12 +3,25 @@ import { Query } from 'react-apollo'
 import { listAuctions } from './graphql/queries';
 import gql from 'graphql-tag';
 import { ListAuctionsQuery, ListAuctionsQueryVariables } from './API';
+import { AuctionCard } from './AuctionCard';
 
 export const Auctions = () => {
     return (
         <Query<ListAuctionsQuery, ListAuctionsQueryVariables> query={gql(listAuctions)}>
             {({data, loading}) => (
-                loading ? null : <div>{JSON.stringify(data?.listAuctions)}</div>
+                loading ||
+                !data ||
+                !data.listAuctions ||
+                !data.listAuctions.items ? null : (
+                    <div style={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(5, 1fr)',
+                        gridGap: 10
+                    }}>
+                        {data.listAuctions.items.map(x => (
+                            <AuctionCard key={x?.id}/>
+                        ))}
+                    </div>)
             )}
         </Query>
     )
