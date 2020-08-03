e82717595c9499ed73296c696220683acf841b01
Larrygf02 <raulgf_02@hotmail.com>
2020-08-02 23:40:02 -0500

    Auction

--- amplify-client/src/App.tsx
+++ amplify-client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { CreateAuctionForm } from './CreateAuctionForm';
+import { Auctions } from './Auction';
 
 function App() {
   return (
     <div className="App">
       <CreateAuctionForm></CreateAuctionForm>
+      <Auctions></Auctions>
     </div>
   );
 }
--- amplify-client/src/Auction.tsx
+++ amplify-client/src/Auction.tsx
@@ -4,11 +4,11 @@ import { listAuctions } from './graphql/queries';
 import gql from 'graphql-tag';
 import { ListAuctionsQuery, ListAuctionsQueryVariables } from './API';
 
-export const Auction = () => {
+export const Auctions = () => {
     return (
         <Query<ListAuctionsQuery, ListAuctionsQueryVariables> query={gql(listAuctions)}>
             {({data, loading}) => (
-                loading ? null : <div>{JSON.stringify(data!.listAuctions)}</div>
+                loading ? null : <div>{JSON.stringify(data?.listAuctions)}</div>
             )}
         </Query>
     )
